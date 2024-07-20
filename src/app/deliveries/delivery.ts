c7983e112571297ce839d7d777c1c6e21066ff57
Ricardo Belfort <rbelfort2004@gmail.com>
2024-07-20 17:54:29 -0400

    feat: criando interface de entrega

---
+++ src/app/deliveries/delivery.ts
@@ -0,0 +1,20 @@
+export interface Delivery {
+  id: string;
+  documento: string;
+  motorista: {
+    nome: string;
+  };
+  cliente_origem: {
+    nome: string;
+    endereco: string;
+    bairro: string;
+    cidade: string;
+  };
+  cliente_destino: {
+    nome: string;
+    endereco: string;
+    bairro: string;
+    cidade: string;
+  };
+  status_entrega: string;
+}
