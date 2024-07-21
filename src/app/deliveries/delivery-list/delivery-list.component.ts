eb1d7ea49607c8af8f3308579e76548ab1a37511
Ricardo Belfort <rbelfort2004@gmail.com>
2024-07-21 12:54:13 -0400

    feat: ajustando arquivo de listagem de entregas

--- src/app/deliveries/delivery-list/delivery-list.component.ts
+++ src/app/deliveries/delivery-list/delivery-list.component.ts
@@ -1,15 +1,47 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Component, inject } from '@angular/core';
-import { DeliveriesService } from '../deliveries.service';
+import { DeliveriesService } from '../../shared/services/deliveries.service';
 import { AsyncPipe, CommonModule } from '@angular/common';
+import { TableModule } from 'primeng/table';
+import { CardModule } from 'primeng/card';
+import { TagModule } from 'primeng/tag';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-delivery-list',
   standalone: true,
-  imports: [AsyncPipe, CommonModule],
+  imports: [
+    AsyncPipe,
+    CommonModule,
+    TableModule,
+    CardModule,
+    TagModule,
+    IconFieldModule,
+    InputIconModule,
+    InputTextModule,
+  ],
   templateUrl: './delivery-list.component.html',
   styleUrl: './delivery-list.component.css',
 })
 export class DeliveryListComponent {
   private service = inject(DeliveriesService);
   deliveries$ = this.service.loadDeliveries();
+
+  getSeverity(status: string) {
+    switch (status) {
+      case 'INSUCESSO':
+        return 'danger';
+
+      case 'ENTREGUE':
+        return 'success';
+
+      case 'PENDENTE':
+        return 'warning';
+
+      default:
+        return undefined;
+    }
+  }
 }
