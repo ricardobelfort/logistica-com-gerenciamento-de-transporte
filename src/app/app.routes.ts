8096160cf3e44ce89f3a4662800716fc343d586a
Ricardo Belfort <rbelfort2004@gmail.com>
2024-07-21 12:53:07 -0400

    feat: ajustes de rotas e arquivos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,14 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'deliveries' },
+  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./dashboard/dashboard.component').then(
+        (d) => d.DashboardComponent,
+      ),
+  },
   {
     path: 'deliveries',
     loadChildren: () =>
